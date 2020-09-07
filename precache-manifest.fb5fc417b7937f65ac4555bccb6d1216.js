d9354020ec21047722167e2c59650ac04ff1d3ae
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-09-07 16:17:04 +0530

    Updates

---
+++ precache-manifest.fb5fc417b7937f65ac4555bccb6d1216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c392abb500b3d07dd3131f2c76227f86",
+    "url": "/netflix-template/index.html"
+  },
+  {
+    "revision": "f73e48cae695814a2cb0",
+    "url": "/netflix-template/static/css/main.3f66f78e.chunk.css"
+  },
+  {
+    "revision": "3c2d73363b140b12f556",
+    "url": "/netflix-template/static/js/2.6feb381a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix-template/static/js/2.6feb381a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73e48cae695814a2cb0",
+    "url": "/netflix-template/static/js/main.829a8b8f.chunk.js"
+  },
+  {
+    "revision": "1d761ed831186f66f189",
+    "url": "/netflix-template/static/js/runtime-main.c35fd963.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/netflix-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
